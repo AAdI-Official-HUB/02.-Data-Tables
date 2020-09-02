c5a8c5e4cffbc07e466944370aaaf558f71a30f1
samirbsh <samirkumar2527@gmail.com>
2020-09-02 15:27:16 +0530

    Updates

---
+++ precache-manifest.fb4f15a4fa55e4d88ab6bf1e91ae8f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbefdb7aeda9c4f2d6fa75485a3902df",
+    "url": "/02.-Data-Tables/index.html"
+  },
+  {
+    "revision": "46f8bcc86e513a152848",
+    "url": "/02.-Data-Tables/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2c3e6b93fbe68eeb13d3",
+    "url": "/02.-Data-Tables/static/css/main.93cac2a9.chunk.css"
+  },
+  {
+    "revision": "46f8bcc86e513a152848",
+    "url": "/02.-Data-Tables/static/js/2.e0bee5dd.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/02.-Data-Tables/static/js/2.e0bee5dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3e6b93fbe68eeb13d3",
+    "url": "/02.-Data-Tables/static/js/main.8dee01de.chunk.js"
+  },
+  {
+    "revision": "59479cb76eea9c6bfa0f",
+    "url": "/02.-Data-Tables/static/js/runtime-main.f3e2ec18.js"
+  }
+]);
